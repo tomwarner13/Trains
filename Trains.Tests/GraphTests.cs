34e722fff712693e0c01947dfbb8d3bf44520be8
tom <tomwarner13@gmail.com>
2021-06-08 19:36:47 -0400

    new project structure

---
+++ Trains.Tests/GraphTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using TrainsData.Schema;
+using Xunit;
+
+namespace Trains.Tests
+{
+  public class GraphTests
+  {
+    [Fact]
+    public void TestGraphBuilding()
+    {
+      var graph = new DirectedGraph();
+      graph.AddNode("A");
+      graph.AddNode("B");
+      
+      Assert.Equal(2, graph.GetNodeNames().Count());
+      
+      graph.AddEdge("A", "B", 3);
+
+      var nodeA = graph.GetNode("A");
+      Assert.Equal(1, nodeA.Edges.Count());
+
+      var edge = nodeA.Edges.First();
+      Assert.Equal(3, edge.Length);
+      Assert.Equal("B", edge.Destination.Name);
+
+      Assert.Throws<ArgumentException>(() => graph.AddEdge("Q", "B", 3));
+      Assert.Throws<ArgumentException>(() => graph.AddEdge("A", "Q", 3));
+      Assert.Throws<ArgumentException>(() => graph.AddEdge("B", "A", -1));
+      Assert.Throws<ArgumentException>(() => graph.AddEdge("A", "B", 3));
+      Assert.Throws<ArgumentException>(() => graph.AddEdge("B", "B", 3));
+    }
+  }
+}
